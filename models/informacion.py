137e7ec4dbb6aa6562d5b0da2d3d52225a793f2e
Gabrielgsd <holagabriel321@gmail.com>
2025-10-21 10:07:34 +0200

    fix(model): Add api.depends for parameters

--- models/informacion.py
+++ models/informacion.py
@@ -12,10 +12,15 @@ class informacion(models.Model):
     alto_cm = fields.Integer(string="Alto en centímetros")
     largo_cm = fields.Integer(string="Largo en centímetros")
     ancho_cm = fields.Integer(string="Ancho en centímetros")
+    volumen = fields.Float(string="Volume:", digits=(6,2), store=True, compute="_volumen")
     peso = fields.Float(string="Peso en Kg.s", digits=(6,2), default=2.7)
     autorizado = fields.Boolean(string="¿Autorizado?", default=False)
     sexo = fields.Selection([ ('male', 'Hombre'), ('female', 'Mujer'), ('other', 'Otro')], string="Sexo")
 
+    @api.depends('alto_cm', 'ancho_cm', 'largo_cm')
+    def _volumen(self):
+        for register in self:
+            register.volumen = (float(register.alto_cm) * float(register.ancho_cm) * float(register.largo_cm)) / 1000000
 
 #     @api.depends('value')
 #     def _value_pc(self):
